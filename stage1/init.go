7672ee34617f3ca96109bcb8298579545fb072d0
Brandon Philips <brandon.philips@coreos.com>
2014-11-29 22:17:39 -0500

    wip

--- stage1/init.go
+++ stage1/init.go
@@ -5,9 +5,10 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 	"syscall"
 
+	"github.com/coreos-inc/rkt/pkg/mount"
+	"github.com/docker/libcontainer/devices"
 	"github.com/coreos-inc/rkt/rkt"
 )
 
@@ -31,14 +32,81 @@ func main() {
 		os.Exit(2)
 	}
 
+	mc := mount.MountConfig{
+		NoPivotRoot: true,
+		DeviceNodes: []*devices.Device{
+			// /dev/null and zero
+			{
+				Path:              "/dev/null",
+				Type:              'c',
+				MajorNumber:       1,
+				MinorNumber:       3,
+				CgroupPermissions: "rwm",
+				FileMode:          0666,
+			},
+			{
+				Path:              "/dev/zero",
+				Type:              'c',
+				MajorNumber:       1,
+				MinorNumber:       5,
+				CgroupPermissions: "rwm",
+				FileMode:          0666,
+			},
+
+			{
+				Path:              "/dev/full",
+				Type:              'c',
+				MajorNumber:       1,
+				MinorNumber:       7,
+				CgroupPermissions: "rwm",
+				FileMode:          0666,
+			},
+
+			// consoles and ttys
+			{
+				Path:              "/dev/tty",
+				Type:              'c',
+				MajorNumber:       5,
+				MinorNumber:       0,
+				CgroupPermissions: "rwm",
+				FileMode:          0666,
+			},
+
+			// /dev/urandom,/dev/random
+			{
+				Path:              "/dev/urandom",
+				Type:              'c',
+				MajorNumber:       1,
+				MinorNumber:       9,
+				CgroupPermissions: "rwm",
+				FileMode:          0666,
+			},
+			{
+				Path:              "/dev/random",
+				Type:              'c',
+				MajorNumber:       1,
+				MinorNumber:       8,
+				CgroupPermissions: "rwm",
+				FileMode:          0666,
+			},
+		},
+	}
+	println(rkt.Stage1RootfsPath(c.Root))
+	if err = mount.InitializeMountNamespace(rkt.Stage1RootfsPath(c.Root), false, &mc); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to initialize mount namespace to %s: %v\n", rkt.Stage1RootfsPath(c.Root), err)
+		os.Exit(2)
+
+	}
+
 	// TODO(philips): compile a static version of systemd-nspawn with this
 	// stupidity patched out
 	_, err = os.Stat("/run/systemd/system")
 	if os.IsNotExist(err) {
 		os.MkdirAll("/run/systemd/system", 0755)
 	}
+	os.MkdirAll("/tmp", 0755)
 
-	ex := filepath.Join(rkt.Stage1RootfsPath(c.Root), nspawnBin)
+	ex := nspawnBin
 	if _, err := os.Stat(ex); err != nil {
 		fmt.Fprintf(os.Stderr, "Failed locating nspawn: %v\n", err)
 		os.Exit(3)
@@ -46,6 +114,7 @@ func main() {
 
 	args := []string{
 		ex,
+		"--help",              // Launch systemd in the container
 		"--boot",              // Launch systemd in the container
 		"--register", "false", // We cannot assume the host system is running a compatible systemd
 	}
@@ -62,6 +131,7 @@ func main() {
 	args = append(args, nsargs...)
 
 	// Arguments to systemd
+	args = append(args, "--help")
 	args = append(args, "--")
 	args = append(args, "--default-standard-output=tty") // redirect all service logs straight to tty
 	if !debug {
@@ -72,7 +142,7 @@ func main() {
 	env := os.Environ()
 
 	if err := syscall.Exec(ex, args, env); err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to execute nspawn: %v\n", err)
-		os.Exit(5)
+		fmt.Fprintf(os.Stderr, "Failed to execute %s %v: %v\n", ex, args, err)
+		os.Exit(6)
 	}
 }
